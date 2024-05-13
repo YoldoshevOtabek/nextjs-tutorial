b13a8589c47ea929f6be908491585fdc8e40d0ea
Ot@bek <148839941+YoldoshevOtabek@users.noreply.github.com>
2024-05-13 06:47:23 +0500

    yangilandi

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,7 +3,7 @@ import '../home.css';
 const Home = () => {
   return (
     <div className='home-container'>
-        <h1>It's first project in Next.js about me</h1>
+        <h1>It is first project in Next.js about me</h1>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import './home.css'
 export default function Home() {
   return (
     <h1 className='home-container'>
-      It's my first project of Next.js
+      It is my first project of Next.js
     </h1>
   );
 }
